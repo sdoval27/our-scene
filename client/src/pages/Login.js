d8f19bc3bcc4c08a14a5fefd18dbe4ef4d97110b
Sinky Li <cityslicker86@gmail.com>
2023-05-25 23:17:49 -0700

    test

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,14 +3,10 @@ import LForm from '../components/LForm'
 
 
 
-const styles = {
-  Card: {}
-  }
-
 
 export default function Login() {
   return(
-    <div style={styles.Card}>                   
+    <div>                   
         <LForm/>
     </div>
 )};
