0bd390cb41bd3c4d17729557f7b00ede64c12c6d
Sinky Li <cityslicker86@gmail.com>
2023-05-25 23:31:45 -0700

    login mutations added

---
+++ client/src/pages/test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Test() {
+    return(
+        <div>
+            THIS IS A TEST
+        </div>
+    )
+
+}
+
+export default Test;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,32 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+
 export const CREATE_POST = gql`
   mutation createPost($content: String!) {
     createPost(content: $content) {
