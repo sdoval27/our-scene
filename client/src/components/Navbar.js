af40ca5a39447ec0f3066a5ef877455bf692fc8f
unknown <sashardoval@gmail.com>
2023-05-30 13:47:05 -0700

    hover features for nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,22 +4,17 @@ import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 
 import SForm from './SForm';
 import LForm from './LForm';
-import Post from '../pages/Post';
 
 //icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faIcons, faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
-import Auth from '../utils/auth';
+//styling
+import './styles/Navbar.css';
 
-const styles = {
-  Center: {
-  textAlign: 'center',
-  justifyContent: 'center',
-  alignItems: 'center',
-},
-}
+//auth
+import Auth from '../utils/auth';
 
 const AppNavbar = ({ currentPage, handlePageChange }) => {
   // set modal display state
@@ -27,55 +22,53 @@ const AppNavbar = ({ currentPage, handlePageChange }) => {
 
   return (
     <>
-      <Navbar bg='dark' variant='dark' expand='lg' style={styles.Center}>
+      <Navbar className="lowernav footer" bg='dark' variant='dark' expand='lg'>
         <Container fluid>
 
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-            <Nav className='ml-auto d-flex'>
-              <a
-                href="/"
-                onClick={() => handlePageChange('Home')}
-                className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
-                {/* link to home page */}
-                <FontAwesomeIcon icon={faIcons} />
-              </a>
-              <a
-                href='/post'
-                onClick={() => handlePageChange('Post')}
-                className={currentPage === 'Post' ? 'nav-link active' : 'nav-link'}>
-                <FontAwesomeIcon icon={faCirclePlus} style={{ color: "#00ffbf", }} />
-              </a>
-              <a
+          <Nav className='center ml-auto d-flex'>
+            <a
+              href="/"
+              onClick={() => handlePageChange('Home')}
+              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+              {/* link to home page */}
+              <FontAwesomeIcon icon={faIcons} />
+            </a>
+            <a
+              href='/post'
+              onClick={() => handlePageChange('Post')}
+              className={currentPage === 'Post' ? 'nav-link active' : 'nav-link'}>
+              <FontAwesomeIcon icon={faCirclePlus} style={{ color: "#00ffbf", }} />
+            </a>
+            <a
               href='/profile'
-              onClick={() => handlePageChange ('Profile')}
+              onClick={() => handlePageChange('Profile')}
               className={currentPage === 'Profile' ? 'nav-link active' : 'nav-link'}>
-                <FontAwesomeIcon icon={faUser} style={{ color: "#f50000", }} />
-              </a>
+              <FontAwesomeIcon icon={faUser} style={{ color: "#f50000", }} />
+            </a>
 
-              {/* if user is logged in show saved books and logout */}
-              {Auth.loggedIn() ? (
-                <>
-                  {/* move link to post page here*/}
-                  <a
-                    href='/post'
-                    onClick={() => handlePageChange('Concerts')}
-                    className={currentPage === 'Concerts' ? 'nav-link active' : 'nav-link'}>
-                    <FontAwesomeIcon icon={faCirclePlus} style={{ color: "#00ffbf", }} />
-                  </a>
+            {/* if user is logged in show saved books and logout */}
+            {Auth.loggedIn() ? (
+              <>
+                {/* move link to post page here*/}
+                <a
+                  href='/post'
+                  onClick={() => handlePageChange('Concerts')}
+                  className={currentPage === 'Concerts' ? 'nav-link active' : 'nav-link'}>
+                  <FontAwesomeIcon icon={faCirclePlus} style={{ color: "#00ffbf", }} />
+                </a>
 
-                  {/* move link to profile page here*/}
-                  <Nav.Link as={Link} to='/profile'>
-                    <FontAwesomeIcon icon={faUser} style={{ color: "#f50000", }} />
-                  </Nav.Link>
+                {/* move link to profile page here*/}
+                <Nav.Link as={Link} to='/profile'>
+                  <FontAwesomeIcon icon={faUser} style={{ color: "#f50000", }} />
+                </Nav.Link>
 
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
+                <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <Nav.Link className="link" onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+            )}
+          </Nav>
+
         </Container>
       </Navbar>
       {/* set modal data up */}
@@ -89,10 +82,10 @@ const AppNavbar = ({ currentPage, handlePageChange }) => {
           <Modal.Header closeButton>
             <Modal.Title id='signup-modal'>
               <Nav variant='pills'>
-                <Nav.Item style={styles.Center}>
+                <Nav.Item >
                   <Nav.Link eventKey='login'>Login</Nav.Link>
                 </Nav.Item>
-                <Nav.Item style={styles.Center}>
+                <Nav.Item >
                   <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
                 </Nav.Item>
               </Nav>
