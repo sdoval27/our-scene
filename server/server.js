37111d4c215eb3436d099e3d17b8e11530dc3495
unknown <sashardoval@gmail.com>
2023-06-05 21:40:53 -0700

    removed cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const multer = require('multer');
 const path = require('path');
 const { authMiddleware } = require ('./utils/auth');
- const cors = require('cors');
+ //const cors = require('cors');
  const http = require('http');
 const socketIO = require('socket.io');
 
@@ -12,7 +12,7 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
- app.use(cors()); // Enable CORS for all routes
+ //app.use(cors()); // Enable CORS for all routes
  const httpServer = http.createServer(app);
 
 
